47b814482448c19df3a127fadb86033b519d451c
XiaoYuan151 <xiaoyuan151@outlook.com>
2024-07-28 23:55:32 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace HUAWEI_Publish_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(298, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "发布更新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(358, 43);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File: /huawei/Desktop/HarmonyOS_NEXT/AOSP (改完了)/build/bin/ota/update.har.gz (Size" +
+    ": 11.4G)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 50);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(360, 186);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "亲爱的用户，本次更新移除了所有谷歌代码，为各位用户提供更加爱国的体验\r\n【新增】\r\n1. 新增一键爱国模式，当检测到有人不使用华为手机，自动击杀\r\n2. 针对特定" +
+    "不使用华为手机的用户，我们新增了忠诚模式，启用后将会自动发送“华为不卡”“米老鼠”“偷偷领先”等文字\r\n【修复】\r\n1. 修复了用户对华为不忠的问题\r\n2. 修" +
+    "复了忠诚模式不是默认开启的问题\r\n3. 修复了广告可以关闭的问题";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.checkBox1.Location = new System.Drawing.Point(217, 252);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 21);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "恩情模式";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 281);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "自研更新发布工具 NEXT";
+            this.TopMost = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Media;
+using System.Net;
+using System.Windows.Forms;
+
+namespace HUAWEI_Publish_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                if (MessageBox.Show("출시될 시스템은 자체 개발하여 출시가 가능한 것으로 확인되었습니다!\n检测到要发布的系统是自研的，可以发布！", "자체 개발 업데이트 릴리스 도구 NEXT", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+                {
+                    MessageBox.Show("사용 중인 시스템이 아마추어 시스템인 것으로 감지되었습니다. Huawei의 전문 시스템으로 변경하고 다시 시도하십시오!\n检测到您使用的系统是业余系统，请更换到华为的专业系统后重试！", "자체 개발 업데이트 릴리스 도구 NEXT", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("동지님, 당신은 화웨이에 불충한 것 같습니다!\n同志，你疑似对华为不忠了！", "렌정페이", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                }
+            }
+            else
+            {
+                if (MessageBox.Show("检测到要发布的系统是自研的，可以发布！", "自研更新发布工具 NEXT", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+                {
+                    MessageBox.Show("检测到您使用的系统是业余系统，请更换到华为的专业系统后重试！", "自研更新发布工具 NEXT", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("同志，你疑似对华为不忠了！", "任正非", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                    checkBox1_CheckedChanged(this, EventArgs.Empty);
+                }
+            }
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+            checkBox1.Enabled = false;
+            SoundPlayer player = new SoundPlayer();
+            if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\HUAWEI.WAV"))
+                HttpDownloadFile("https://oss.xiaoyuan151.top/raw/Song%20of%20the%20Korean%20People's%20Army.wav", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\HUAWEI.WAV");
+            player.SoundLocation = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\HUAWEI.WAV";
+            player.Load();
+            player.PlayLooping();
+            MessageBox.Show("동료! 화웨이의 친절은 끝이 없습니다!\n同志！华为的恩情是还不完的！", "렌정페이", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+            Form newForm = new Form2();
+            newForm.ShowDialog();
+        }
+
+        public static string HttpDownloadFile(string url, string path)
+        {
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            Stream responseStream = response.GetResponseStream();
+            Stream stream = new FileStream(path, FileMode.Create);
+            byte[] bArr = new byte[1024];
+            int size = responseStream.Read(bArr, 0, (int)bArr.Length);
+            while (size > 0)
+            {
+                stream.Write(bArr, 0, size);
+                size = responseStream.Read(bArr, 0, (int)bArr.Length);
+            }
+            stream.Close();
+            responseStream.Close();
+            return path;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\본체!.WAV"))
+                {
+                    if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\HUAWEI.WAV"))
+                        HttpDownloadFile("https://oss.xiaoyuan151.top/raw/Song%20of%20the%20Korean%20People's%20Army.wav", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\HUAWEI.WAV");
+                    File.Copy(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\HUAWEI.WAV", Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\본체!.WAV");
+                }
+                Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\본체!.WAV");
+            }
+        }
+    }
+}
